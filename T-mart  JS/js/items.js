1bd71d8e3d8add15df9c80d41dc7599cc9b1d9d1
kimtaehyun304 <162421607+kimtaehyun304@users.noreply.github.com>
2024-06-19 20:50:55 +0900

    파일 업로드

---
+++ T-mart  JS/js/items.js
@@ -0,0 +1,14 @@
+let items = {
+    // 앞의 숫자는 pageId 입니다
+    1: [
+        { pageId: 1, itemId: 1, name: "너구리", price: "1000", seller: "농심", description: "너구리 한마리 몰고 가슈", quantity: 3 },
+        { pageId: 1, itemId: 2, name: "김치 사발면", price: "900", seller: "농심", description: "한국인은 김치를 먹어야죠", quantity: 4 },
+    ],
+    2: [
+        { pageId: 2, itemId: 3, name: "둥지 냉면", price: "1500", seller: "농심", description: "여름엔 시원한 냉면 강추", quantity: 5 }
+    ],
+    3: [
+        { pageId: 3, itemId: 4, name: "맛짬뽕", price: "1200", seller: "농심", description: "얼큰한 짬뽕입니다", quantity: 6 }
+    ]
+
+};
